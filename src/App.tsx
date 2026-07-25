0ad8de7c6b0642163a3e627f0c0551db06ef8b63
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-25 19:31:14 +0200

    Align project elements on cross axis

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ function App() {
         <div className="border-b border-secondary pb-2">
           <h2 className="text-secondary">PROJECTS</h2>
         </div>
-        <div className="border-b border-secondary flex justify-between py-4">
+        <div className="border-b border-secondary flex justify-between items-center py-4">
           <p>meg-zki</p>
           <p className="text-secondary text-xs">PORTFOLIO WEBSITE</p>
         </div>
-        <div className="border-b border-secondary flex justify-between py-4">
+        <div className="border-b border-secondary flex justify-between items-center py-4">
           <p>gasmetrics</p>
           <p className="text-secondary text-xs">GAS CONSUMPTION TRACKER</p>
         </div>
