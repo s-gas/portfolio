af4aed3f8365f52dde25152d71d7de61fd40bade
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-28 12:22:11 +0200

    Update linkedin URL

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="flex flex-col gap-4 sm:flex-row justify-between mt-60 text-sm text-secondary">
       <Link href="https://github.com/s-gas">GITHUB</Link>
-      <Link href="https://www.linkedin.com/in/simone-gasparini-0b95a5179">LINKEDIN</Link>
+      <Link href="https://www.linkedin.com/in/s-gas">LINKEDIN</Link>
       <Link href="mailto:simone.gasparini.2@gmail.com">EMAIL</Link>
       <p>© {new Date().getFullYear()}</p>
     </footer>
